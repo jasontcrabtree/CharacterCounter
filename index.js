103a01d47d0cdb8bd652a80a91fb08366146954b
Jason <jasontcrabtree@gmail.com>
2018-12-29 23:54:17 +1300

    css changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-console.log("app is ready");
 var inputElement = document.getElementById("textAreaInput");
 inputElement.addEventListener("keyup", function(e) {
   theirInput = e.target.value;
-  document.getElementById("text").innerHTML = theirInput.length;
+  document.getElementById("number").innerHTML = theirInput.length;
 });
