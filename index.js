4502fd87b1a6c767a8c9d22354c4ddc6e30e5881
Jason <jasontcrabtree@gmail.com>
2020-01-02 18:17:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ inputElement.addEventListener('keyup', function(e) {
 	theirInput = e.target.value;
 	document.getElementById('number').innerHTML = theirInput.length;
 });
+
+// function logChangeToTextArea() {
+//   const textArea = document.querySelector('#textArea').value.length;
+//   console.log(textArea);
+// }
